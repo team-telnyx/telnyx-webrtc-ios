bee23c0988f0c6e93b1ecdc4fafdcaa1a03e6202
Guillermo Battistel <guillermo@telnyx.com>
2021-03-09 15:42:01 -0300

    (WEBRTC-420): Ringtone / ringback tone support
    Added support to the SDK to set a custom ringtone and/or ringback tone

--- WebRTCSDK/Telnyx/Models/TxConfig.swift
+++ WebRTCSDK/Telnyx/Models/TxConfig.swift
@@ -13,22 +13,31 @@ public struct TxConfig {
     var password: String?
     var token: String?
 
+    var ringBackTone: String?
+    var ringtone: String?
 
-    /// Constructor of the Telnyx SDK configuration
-    /// Login using sip user  and password.
-    ///
+
+    /// Constructor of the Telnyx SDK configuration: Login using sip user  and password.
     /// - Parameters:
     ///   - sipUser: sipUser the SIP user
     ///   - password: password the password of the SIP user.
-    public init(sipUser: String, password: String) {
+    ///   - ringtone: (Optional) The audio file name to be played when receiving an incoming call. e.g.: "my-ringtone.mp3"
+    ///   - ringBackTone: (Optional) The audio file to be played when calling. e.g.: "my-ringbacktone.mp3"
+    public init(sipUser: String, password: String, ringtone: String? = nil, ringBackTone: String? = nil) {
         self.sipUser = sipUser
         self.password = password
+        self.ringBackTone = ringBackTone
+        self.ringtone = ringtone
     }
 
-    /// Constructor of the Telnyx SDK configuration
-    /// Login using a token.
-    /// - Parameter token: Token generated from https://developers.telnyx.com/docs/v2/webrtc/quickstart
-    public init(token: String) {
+    /// Constructor of the Telnyx SDK configuration: Login using a token.
+    /// - Parameters:
+    ///   - token: Token generated from https://developers.telnyx.com/docs/v2/webrtc/quickstart
+    ///   - ringtone: (Optional) The audio file name to be played when receiving an incoming call. e.g.: "my-ringtone.mp3"
+    ///   - ringBackTone: (Optional) The audio file name to be played when calling. e.g.: "my-ringbacktone.mp3"
+    public init(token: String, ringtone: String? = nil, ringBackTone: String? = nil) {
         self.token = token
+        self.ringBackTone = ringBackTone
+        self.ringtone = ringtone
     }
 }
