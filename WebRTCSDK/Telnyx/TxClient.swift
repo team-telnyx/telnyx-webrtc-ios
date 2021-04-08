8ee79385d20efad567b75682fbde169f839c33c0
Guillermo Battistel <guillermo@telnyx.com>
2021-04-08 18:14:10 -0300

    (WEBRTC-474) TxClient class docs improvements
    Added examples of usage

--- WebRTCSDK/Telnyx/TxClient.swift
+++ WebRTCSDK/Telnyx/TxClient.swift
@@ -11,21 +11,131 @@ import AVFoundation
 
 /// The `TelnyxRTC` client connects your application to the Telnyx backend,
 /// enabling you to make outgoing calls and handle incoming calls.
+///
+/// ## Examples
+/// ### Connect and login:
+///
+/// ```
+/// // Initialize the client
+/// let telnyxClient = TxClient()
+///
+/// // Register to get SDK events
+/// telnyxClient.delegate = self
+///
+/// // Setup yor connection parameters.
+///
+/// // Set the login credentials and the ringtone/ringback configurations if required.
+/// // Ringtone / ringback tone files are not mandatory.
+/// // You can user your sipUser and password
+/// let txConfigUserAndPassowrd = TxConfig(sipUser: sipUser,
+///                                        password: password,
+///                                        ringtone: "incoming_call.mp3",
+///                                        ringBackTone: "ringback_tone.mp3",
+///                                        //You can choose the appropriate verbosity level of the SDK.
+///                                        //Logs are disabled by default
+///                                        logLevel: .all)
+///
+/// // Use a JWT Telnyx Token to authenticate (recommended)
+/// let txConfigToken = TxConfig(token: "MY_JWT_TELNYX_TOKEN",
+///                              ringtone: "incoming_call.mp3",
+///                              ringBackTone: "ringback_tone.mp3",
+///                              //You can choose the appropriate verbosity level of the SDK. Logs are disabled by default
+///                              logLevel: .all)
+///
+/// do {
+///    // Connect and login
+///    // Use `txConfigUserAndPassowrd` or `txConfigToken`
+///    try telnyxClient.connect(txConfig: txConfigToken)
+/// } catch let error {
+///    print("ViewController:: connect Error \(error)")
+/// }
+///
+/// // You can call client.disconnect() when you're done.
+/// Note: you need to relese the delegate manually when you are done.
+///
+/// // Disconnecting and Removing listeners.
+/// telnyxClient.disconnect();
+///
+/// // Release the delegate
+/// telnyxClient.delegate = nil
+///
+/// ```
+///
+/// ### Listen TxClient delegate events.
+///
+/// ```
+/// extension ViewController: TxClientDelegate {
+///
+///     func onRemoteCallEnded(callId: UUID) {
+///         // Call has been removed internally.
+///     }
+///
+///     func onSocketConnected() {
+///        // When the client has successfully connected to the Telnyx Backend.
+///     }
+///
+///     func onSocketDisconnected() {
+///        // When the client from the Telnyx backend
+///     }
+///
+///     func onClientError(error: Error)  {
+///         // Something went wrong.
+///     }
+///
+///     func onClientReady()  {
+///        // You can start receiving incoming calls or
+///        // start making calls once the client was fully initialized.
+///     }
+///
+///     func onSessionUpdated(sessionId: String)  {
+///        // This function will be executed when a sessionId is received.
+///     }
+///
+///     func onIncomingCall(call: Call)  {
+///        // Someone is calling you.
+///     }
+///
+///     // You can update your UI from here base on the call states.
+///     // Check that the callId is the same as your current call.
+///     func onCallStateUpdated(callState: CallState, callId: UUID) {
+///         DispatchQueue.main.async {
+///             switch (callState) {
+///             case .CONNECTING:
+///                 break
+///             case .RINGING:
+///                 break
+///             case .NEW:
+///                 break
+///             case .ACTIVE:
+///                 break
+///             case .DONE:
+///                 break
+///             case .HELD:
+///                 break
+///             }
+///         }
+///     }
+/// }
+/// ```
 public class TxClient {
 
+    // MARK: - Properties
     /// Keeps track of all the created calls by theirs UUIDs
     public internal(set) var calls: [UUID: Call] = [UUID: Call]()
-    /// Subscribe to TxClient delegate to receive Telnyx RTC events
+    /// Subscribe to TxClient delegate to receive Telnyx SDK events
     public var delegate: TxClientDelegate?
     private var socket : Socket?
 
     private var sessionId : String?
     private var txConfig: TxConfig?
 
+    // MARK: - Constructor
+    /// TxClient has to be instantiated.
     public init() {}
 
+    // MARK: - Connection handling
     /// Connects to the iOS client to the Telnyx signaling server using the desired login credentials.
-    /// - Parameter txConfig: txConfig. The desired login credentials. See TxConfig docummentation for more information.
+    /// - Parameter txConfig: The desired login credentials. See TxConfig docummentation for more information.
     /// - Throws: TxConfig parameters errors
     public func connect(txConfig: TxConfig) throws {
         Logger.log.i(message: "TxClient:: connect()")
@@ -46,17 +156,17 @@ public class TxClient {
         delegate?.onSocketDisconnected()
     }
 
-    /// Obtaian the current session ID after loggin in to Telnyx server.
-    /// - Returns: The current sessionId. If this value is empty, that means that the client is not connected to Telnyx server.
-    public func getSessionId() -> String {
-        return sessionId ?? ""
-    }
-
-    /// Check if TxClient is connected to Telnyx servers.
+    /// To check if TxClient is connected to Telnyx server.
     /// - Returns: `true` if TxClient socket is connected, `false` otherwise.
     public func isConnected() -> Bool {
         guard let isConnected = socket?.isConnected else { return false }
         return isConnected
+    }
+
+    /// Get the current session ID after logging into Telnyx Backend.
+    /// - Returns: The current sessionId. If this value is empty, that means that the client is not connected to Telnyx server.
+    public func getSessionId() -> String {
+        return sessionId ?? ""
     }
 }
 
@@ -71,7 +181,7 @@ extension TxClient {
         return self.calls[callId]
     }
 
-    /// Creates a Call and starts the call sequence, negotiate the ICE Candidates and sends the invite.
+    /// Creates a new Call and starts the call sequence, negotiate the ICE Candidates and sends the invite.
     /// - Parameters:
     ///   - callerName: The caller name. This will be displayed as the caller name in the remote's client.
     ///   - callerNumber: The caller Number. The phone number of the current user.
@@ -112,6 +222,7 @@ extension TxClient {
         self.calls[callId] = call
         return call
     }
+
 
     fileprivate func createIncomingCall(callerName: String, callerNumber: String, callId: UUID, remoteSdp: String) {
 
