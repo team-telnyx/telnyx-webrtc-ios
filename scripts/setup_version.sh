f9bf6d7f11aa395bc86e29da2430e80067f1a46e
Guillermo Battistel <guillermo@telnyx.com>
2021-10-19 11:13:42 -0300

    (WEBRTC-705) Added bash script to update the SDK version

---
+++ scripts/setup_version.sh
@@ -0,0 +1,15 @@
+# This script updates the SDK version in the required files
+# Example of usage: sh scripts/setup_version.sh -v "1.0.0"
+#!/bin/bash
+while getopts v: flag
+do
+    case "${flag}" in
+        v) version=${OPTARG};;
+    esac
+done
+echo "New version: $version";
+
+# Replace version in podspec file
+sed -i '' 's/spec.version = .*/spec.version = "'$version'"/' TelnyxRTC.podspec
+# Replace version in info.plist file
+sed -i '' 's/MARKETING_VERSION = .*/MARKETING_VERSION = '"$version"';/' TelnyxRTC.xcodeproj/project.pbxproj
