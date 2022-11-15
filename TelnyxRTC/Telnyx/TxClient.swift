bbed3f9723405abd0fab063878e237031357090d
Ashish David <ashish.ultivic@gmail.com>
2022-11-15 14:13:51 +0530

    initial push

--- TelnyxRTC/Telnyx/TxClient.swift
+++ TelnyxRTC/Telnyx/TxClient.swift
@@ -618,7 +618,10 @@ extension TxClient : SocketDelegate {
                                                 telnyxLegId: telnyxLegId)
                     }
                     break;
-
+            case .PING:
+                print("inside ping send pong")
+                self.socket?.sendMessage(message: message)
+                break;
                 default:
                     Logger.log.i(message: "TxClient:: SocketDelegate Default method")
                     break
