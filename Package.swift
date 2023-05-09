15fa0f8e604ea822108cd1ff8793d5a63d344dfd
“Rakshak” <rakshakgaind0910@gmail.com>
2023-05-09 16:10:36 +0530

    POD update

--- Package.swift
+++ Package.swift
@@ -6,14 +6,20 @@ import PackageDescription
 let package = Package(
     name: "TelnyxRTC",
     platforms: [.iOS(.v13)],
-    products: [.library(name: "TelnyxRTC", targets: ["Telnyx"])],
+    products: [.library(name: "TelnyxRTC", targets: ["TelnyxRTC"])],
+    dependencies: [
+        .package(url: "https://github.com/bugsnag/bugsnag-cocoa.git", from: "6.26.2"),
+        .package(url: "https://github.com/daltoniam/Starscream.git", from: "3.1.1"),
+        .package(url: "https://github.com/stasel/WebRTC.git", branch: "latest")
+    ],
     targets: [
         .target(
-            name: "Telnyx",
+            name: "TelnyxRTC",
             path: "Sources",
             exclude: ["Info.plsit"]),
         .testTarget(
-            name: "TelnyxRTCTests",
+            name: "TelnyxTests",
+            dependencies: ["TelnyxRTC"],
             path: "Tests",
             exclude: ["Info.plist"]),
     ],
