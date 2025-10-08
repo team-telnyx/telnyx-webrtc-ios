f66a5c190f0a1e54ee0790d53194e44200956a9f
gbattistel <gbattistel@users.noreply.github.com>
2025-10-08 15:27:46 +0000

    🚀 chore: release v2.2.2

--- TelnyxRTC/Telnyx/Verto/Message.swift
+++ TelnyxRTC/Telnyx/Verto/Message.swift
@@ -13,7 +13,7 @@ private let PROTOCOL_VERSION: String = "2.0"
 
 class Message {
     internal static let CLIENT_TYPE = "iOS"
-    internal static let SDK_VERSION = "2.2.1"
+    internal static let SDK_VERSION = "2.2.2"
     internal static var USER_AGENT: String {
         get {
             let type = Message.CLIENT_TYPE
