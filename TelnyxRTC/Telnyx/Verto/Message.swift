23dc2265c380e051fb7330b3a7f6369c1fa215b3
isaacakakpo1 <isaacakakpo1@users.noreply.github.com>
2026-05-24 23:21:58 +0000

    🚀 chore: release v4.0.1

--- TelnyxRTC/Telnyx/Verto/Message.swift
+++ TelnyxRTC/Telnyx/Verto/Message.swift
@@ -13,7 +13,7 @@ private let PROTOCOL_VERSION: String = "2.0"
 
 class Message {
     internal static let CLIENT_TYPE = "iOS"
-    internal static let SDK_VERSION = "4.0.0"
+    internal static let SDK_VERSION = "4.0.1"
     internal static var USER_AGENT: String {
         get {
             return userAgent(enableMissedCallNotifications: false)
